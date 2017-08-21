8e09a309d52ce81008e5ea024fff7579091aee44
Matt Gathu <mattgathu@gmail.com>
2017-08-21 12:19:35 +0300

    [WIP] code refactoring
    * split logic into multiple functions
        - url parsing
        - saving contents into file
    * adding error handling
        - using Result type
    * bumped up crate version

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,49 @@ extern crate indicatif;
 use std::fs::File;
 use std::io::Read;
 use std::io::copy;
-use reqwest::Client;
-use reqwest::header::ContentLength;
-use indicatif::{ProgressBar, ProgressStyle};
+use reqwest::{Client, Url, UrlError};
+use reqwest::header::{ContentLength, ContentType};
+use indicatif::{ProgressBar, ProgressStyle, HumanBytes};
 use clap::{Arg, App};
 use console::style;
 
-fn download(target: &str) {
+fn parse_url(url: &str) -> Result<Url, UrlError> {
+    match Url::parse(url) {
+        Ok(url) => Ok(url),
+        Err(error) if error == UrlError::RelativeUrlWithoutBase => {
+            let url_with_base = format!("{}{}", "http://", url);
+            Url::parse(url_with_base.as_str())
+        }
+        Err(error) => return Err(error),
+    }
 
+}
+
+fn download(target: &str) -> Result<(), Box<::std::error::Error>> {
+
+    // parse url
+    let url = parse_url(target)?;
     let client = Client::new().unwrap();
-    let mut resp = client.get(target)
-        .unwrap()
+    let mut resp = client.get(url)?
         .send()
         .unwrap();
+    println!("HTTP request sent... {}",
+             style(format!("{}", resp.status())).green());
     if resp.status().is_success() {
 
-        let len = resp.headers()
-            .get::<ContentLength>()
-            .map(|ct_len| **ct_len)
-            .unwrap_or(0);
+        let headers = resp.headers().clone();
+        let len = headers.get::<ContentLength>().map(|ct_len| **ct_len).unwrap_or(0);
+
+        let ct_type = headers.get::<ContentType>().unwrap();
+
+        println!("Length: {} ({})",
+                 style(len).green(),
+                 style(format!("{}", HumanBytes(len))).red());
+        println!("Type: {}", style(ct_type).green());
 
         let fname = target.split("/").last().unwrap();
-        println!("Downloading: '{}' Size: {}b",
-                 style(fname).bold(),
-                 style(len).bold());
+
+        println!("Saving to: {}", style(fname).green());
 
         let chunk_len = len as usize / 99;
         let mut buf = Vec::new();
@@ -58,25 +77,34 @@ fn download(target: &str) {
 
         bar.finish();
 
-        let mut file = File::create(fname).unwrap();
-        copy(&mut buf.as_slice(), &mut file).unwrap();
+        save_to_file(&mut buf, fname)?;
     }
+
+    Ok(())
 
 }
 
+fn save_to_file(contents: &mut Vec<u8>, fname: &str) -> Result<(), std::io::Error> {
+    let mut file = File::create(fname).unwrap();
+    copy(&mut contents.as_slice(), &mut file).unwrap();
+    Ok(())
+
+}
 
 fn main() {
     let matches = App::new("My Simple Downloader")
         .version("0.1.0")
         .author("Matt Gathu <mattgathu@gmail.com>")
         .about("Progressful downloader")
-        .arg(Arg::with_name("url")
+        .arg(Arg::with_name("URL")
                  .required(true)
-                 .short("u")
-                 .long("url")
                  .takes_value(true)
+                 .index(1)
                  .help("url to download"))
         .get_matches();
-    let url = matches.value_of("url").unwrap();
-    download(url);
+    let url = matches.value_of("URL").unwrap();
+    match download(url) {
+        Ok(_) => println!("Download Successful!"),
+        Err(e) => println!("Got error: {}", e.description()),
+    }
 }
