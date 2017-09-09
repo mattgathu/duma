4450b317016a63dcf241bc349cc7eb2cea97d48b
Matt Gathu <mattgathu@gmail.com>
2017-09-09 08:47:16 +0300

    added message when resuming completed download
    msg: `The file is already fully retrieved; nothing to do.`

--- src/main.rs
+++ src/main.rs
@@ -159,6 +159,8 @@ fn download(target: &str, quiet_mode: bool, filename: Option<&str>, resume_downl
 
         bar.finish();
 
+    } else if resp.status().as_u16() == 416 {
+        print(format!("\n{}\n", style("The file is already fully retrieved; nothing to do.").red()), quiet_mode);
     }
 
     Ok(())
