c2095d074894bb261a2188988706fdfa154d4d4f
Matt Gathu <mattgathu@gmail.com>
2017-09-11 04:55:57 +0300

    improved test
    * used assert_eq for more accuracy

--- tests/test.rs
+++ tests/test.rs
@@ -2,11 +2,19 @@ use std::process::Command;
 
 #[test]
 fn calling_rget_without_args() {
+    let expected = r#"error: The following required arguments were not provided:
+    <URL>
+
+USAGE:
+    rget [FLAGS] [OPTIONS] <URL>
+
+For more information try --help
+"#;
     let output = Command::new("sh")
         .arg("-c")
         .arg("./target/debug/rget")
         .output()
         .expect("failed to execute process");
 
-    assert!(String::from_utf8_lossy(&output.stderr).contains("error: The following required arguments were not provided:"));
+    assert_eq!(String::from_utf8_lossy(&output.stderr), expected);
 }
