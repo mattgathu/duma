3e2e34a21e96c84c4064ad8393cc55216b89cf16
Matt Gathu <mattgathu@gmail.com>
2017-09-11 06:22:31 +0300

    updated test organization

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,30 +1,39 @@
 use std::process::Command;
 
-#[test]
-fn calling_rget_without_args() {
-    let expected = r#"error: The following required arguments were not provided:
+static WITHOUT_ARGS_OUTPUT: &'static str = "error: The following required arguments were not provided:
     <URL>
 
 USAGE:
     rget [FLAGS] [OPTIONS] <URL>
 
 For more information try --help
-"#;
-    let output = Command::new("./target/debug/rget")
-        .output()
-        .expect("failed to execute process");
+";
 
-    assert_eq!(String::from_utf8_lossy(&output.stderr), expected);
-}
+static INVALID_URL_OUTPUT: &'static str = "Got error: failed to lookup address information: nodename nor servname provided, or not known
+";
+ 
+#[cfg(test)]
+mod integration {
+    use Command;
+    use WITHOUT_ARGS_OUTPUT;
+    use INVALID_URL_OUTPUT;
 
-#[test]
-fn calling_rget_with_invalid_url() {
-    let expected = r#"Got error: failed to lookup address information: nodename nor servname provided, or not known
-"#;
-    let output = Command::new("./target/debug/rget")
-        .arg("wwww.shouldnotwork.com")
-        .output()
-        .expect("failed to execute process");
-
-    assert_eq!(String::from_utf8_lossy(&output.stderr), expected);
+    #[test]
+    fn calling_rget_without_args() {
+        let output = Command::new("./target/debug/rget")
+            .output()
+            .expect("failed to execute process");
+    
+        assert_eq!(String::from_utf8_lossy(&output.stderr), WITHOUT_ARGS_OUTPUT);
+    }
+    
+    #[test]
+    fn calling_rget_with_invalid_url() {
+        let output = Command::new("./target/debug/rget")
+            .arg("wwww.shouldnotwork.com")
+            .output()
+            .expect("failed to execute process");
+    
+        assert_eq!(String::from_utf8_lossy(&output.stderr), INVALID_URL_OUTPUT);
+    }
 }
